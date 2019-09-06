babea776867f68efadfe063e763ecdfa50ab53ed
Alexey Golub <Tyrrrrrr@gmail.com>
2019-09-06 19:32:22 +0300

    Refactor meta

--- src/components/meta.jsx
+++ src/components/meta.jsx
@@ -1,52 +1,36 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-export const Meta = ({ title, description }) => {
-  title =
-    title === null || title === undefined
-      ? `Alexey Golub`
-      : `${title} | Alexey Golub`
+const normalizeTitle = (title) => {
+  if (title === null || title === undefined)
+    return `Alexey Golub`
 
-  description = description || `Alexey Golub (@tyrrrz) is a software developer, open source maintainer, tech blogger and conference speaker`
+  return `${title} | Alexey Golub`
+}
+
+export const Meta = ({ title, description }) => {
+  title = normalizeTitle(title)
+
+  description = description ||
+    `Alexey Golub (@tyrrrz) is a software developer, open source maintainer, tech blogger and conference speaker`
 
   return (
-    <Helmet
-      htmlAttributes={{ lang: `en` }}
-      title={title}
-      meta={[
-        {
-          name: `description`,
-          content: description,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: description,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: `@Tyrrrz`,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: description,
-        },
-      ]}
-    />
+    <Helmet>
+      <html lang='en' />
+
+      <title>{title}</title>
+
+      <meta property='og:title' content={title} />
+      <meta name='twitter:title' content={title} />
+
+      <meta property='og:type' content='website' />
+
+      <meta name='twitter:creator' content='@Tyrrrz' />
+      <meta name='twitter:card' content='summary' />
+
+      <meta name='description' content={description} />
+      <meta property='og:description' content={description} />
+      <meta name='twitter:description' content={description} />
+    </Helmet>
   )
 }
