b00660d77cb35769cd3d433d170d03f0c0451da0
Alexey Golub <Tyrrrrrr@gmail.com>
2019-09-17 16:35:18 +0300

    Ensure domain ends with slash

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,7 @@
+const ensureEndsWithSlash = str => !str.endsWith('/') ? str + '/' : str;
+
 module.exports = {
-  siteDomain: process.env.TYRRRZME_DOMAIN || `https://tyrrrz.me/`,
+  siteDomain: ensureEndsWithSlash(process.env.TYRRRZME_DOMAIN || `https://tyrrrz.me/`),
   githubToken: process.env.TYRRRZME_GITHUB,
   disqusId: process.env.TYRRRZME_DISQUS,
   googleAnalyticsId: process.env.TYRRRZME_GOOGLEANALYTICS
