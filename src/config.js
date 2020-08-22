a948888f7fa9e092a887e6d1a81682add3ff5363
Alexey Golub <tyrrrz@gmail.com>
2020-08-22 23:05:02 +0300

    Improve URL env var

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteDomain: process.env.GATSBY_URL || 'http://localhost:8000',
+  siteDomain: process.env.URL || process.env.GATSBY_URL || 'http://localhost:8000',
   disqusId: process.env.GATSBY_DISQUS,
   googleAnalyticsId: process.env.GATSBY_GOOGLEANALYTICS
 };
