38e7e1ca29c3abd1e255d8c7737d71c8849b73a0
Alexey Golub <tyrrrz@gmail.com>
2020-08-22 22:21:50 +0300

    Use Netlify-provided URL in config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteDomain: process.env.TYRRRZME_DOMAIN || 'http://localhost:8000',
+  siteDomain: process.env.URL || process.env.TYRRRZME_DOMAIN || 'http://localhost:8000',
   disqusId: process.env.TYRRRZME_DISQUS,
   googleAnalyticsId: process.env.TYRRRZME_GOOGLEANALYTICS,
   clickyId: process.env.TYRRRZME_CLICKY
