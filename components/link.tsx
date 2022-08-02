7da91007464a05f28d0eace55e55ac7214087d6c
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-08-02 21:46:22 +0300

    asd

--- components/link.tsx
+++ components/link.tsx
@@ -3,44 +3,35 @@ import c from 'classnames';
 import NextLink from 'next/link';
 import { FC, PropsWithChildren } from 'react';
 
-type RawLinkProps = PropsWithChildren<{
-  className?: string;
-  href: string;
-}>;
-
-const RawLink: FC<RawLinkProps> = ({ className, href, children }) => {
-  if (isAbsoluteUrl(href)) {
-    return (
-      <a className={className} href={href} target="_blank" rel="noreferrer">
-        {children}
-      </a>
-    );
-  } else {
-    return (
-      <NextLink href={href} passHref>
-        <a className={className}>{children}</a>
-      </NextLink>
-    );
-  }
-};
-
 type LinkProps = PropsWithChildren<{
   variant?: 'normal' | 'discreet' | 'hidden';
   href: string;
 }>;
 
 const Link: FC<LinkProps> = ({ variant = 'normal', href, children }) => {
-  return (
-    <RawLink
+  const isAbsolute = isAbsoluteUrl(href);
+
+  const rawLink = (
+    <a
       className={c({
         'text-blue-500': variant === 'normal',
         'hover:underline': variant === 'normal',
         'hover:text-blue-500': variant === 'discreet'
       })}
       href={href}
+      target={isAbsolute ? '_blank' : undefined}
+      rel="noreferrer"
     >
       {children}
-    </RawLink>
+    </a>
+  );
+
+  return isAbsolute ? (
+    rawLink
+  ) : (
+    <NextLink href={href} passHref>
+      {rawLink}
+    </NextLink>
   );
 };
 
