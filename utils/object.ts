b67a5333e8e575345c46ca2648202561415f38da
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-08-05 22:44:11 +0300

    Fix nullref error

--- utils/object.ts
+++ utils/object.ts
@@ -2,7 +2,7 @@ export const deleteUndefined = (obj: any) => {
   for (const key of Object.keys(obj)) {
     if (typeof obj[key] === 'undefined') {
       delete obj[key];
-    } else if (typeof obj[key] === 'object') {
+    } else if (typeof obj[key] === 'object' && obj[key] !== null) {
       deleteUndefined(obj[key]);
     }
   }
