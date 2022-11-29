0a25a0b75149d8d95b938dc410f1b16605ecc258
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-11-29 18:27:37 +0200

    Remove nowrap from code blocks

--- components/code.tsx
+++ components/code.tsx
@@ -4,11 +4,7 @@ import { FC, PropsWithChildren } from 'react';
 type CodeProps = PropsWithChildren;
 
 const Code: FC<CodeProps> = ({ children }) => {
-  return (
-    <code className={c('px-1', 'bg-neutral-100', 'text-sm', 'font-mono', 'whitespace-nowrap')}>
-      {children}
-    </code>
-  );
+  return <code className={c('px-1', 'bg-neutral-100', 'text-sm', 'font-mono')}>{children}</code>;
 };
 
 export default Code;
