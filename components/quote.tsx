070d0029bc135e07c93a9ac329520c54358fcbcf
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-01-21 01:58:17 +0200

    Normalize colors

--- components/quote.tsx
+++ components/quote.tsx
@@ -5,7 +5,7 @@ type QuoteProps = PropsWithChildren;
 
 const Quote: FC<QuoteProps> = ({ children }) => {
   return (
-    <blockquote className={c('px-4', 'py-1', 'border-l-4', 'border-purple-500', 'bg-purple-50')}>
+    <blockquote className={c('px-4', 'py-1', 'border-l-4', 'border-purple-500', 'bg-purple-100')}>
       {children}
     </blockquote>
   );
