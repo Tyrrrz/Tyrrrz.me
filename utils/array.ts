ae6988a684ce191bccd17fd7702a4ef995f6b73a
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-03-14 18:08:19 +0200

    Fix more GitHub Sponsors issues

--- utils/array.ts
+++ utils/array.ts
@@ -10,3 +10,14 @@ export const groupBy = <T, K>(array: T[], getKey: (item: T) => K) => {
 
   return [...map.entries()].map(([key, items]) => ({ key, items }));
 };
+
+export const distinctBy = <T, K>(array: T[], getKey: (item: T) => K) => {
+  const map = array.reduce((acc, item) => {
+    const key = getKey(item);
+    acc.set(key, item);
+
+    return acc;
+  }, new Map<K, T>());
+
+  return [...map.values()];
+};
