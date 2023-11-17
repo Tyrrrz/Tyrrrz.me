a5408d99001a54c155eeb75f21ea72b37a47a9af
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-18 01:05:30 +0200

    Generalize `ustLocalState` hook

--- hooks/useLocalState.ts
+++ hooks/useLocalState.ts
@@ -1,66 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
-
-const getStorageValue = (key: string) => {
-  const item = localStorage.getItem(key);
-
-  if (typeof item !== 'undefined' && item !== null) {
-    try {
-      return JSON.parse(item);
-    } catch {
-      return null;
-    }
-  }
-
-  return null;
-};
-
-const setStorageValue = (key: string, value: any) => {
-  localStorage.setItem(key, JSON.stringify(value));
-
-  // Storage events are not triggered within the same page that modified the storage,
-  // so we need to dispatch the event manually to make sure that other hooks get notified.
-  dispatchEvent(new StorageEvent('storage'));
-};
+import useBrowserState from '~/hooks/useBrowserState';
 
 const useLocalState = <T>(key: string, initialState: T) => {
-  const isMounted = useRef(false);
-  const [value, setValue] = useState<T>(initialState);
-
-  // Initial value from the storage
-  useEffect(() => {
-    const item = getStorageValue(key);
-    if (item) {
-      setValue(item);
-    }
-
-    return () => {
-      isMounted.current = false;
-    };
-  }, [key]);
-
-  // Value changed by the consumer
-  useEffect(() => {
-    if (isMounted.current) {
-      setStorageValue(key, value);
-    } else {
-      isMounted.current = true;
-    }
-  }, [key, value]);
-
-  // Value changed in the storage
-  useEffect(() => {
-    const onChange = () => {
-      setValue(getStorageValue(key));
-    };
-
-    addEventListener('storage', onChange);
-
-    return () => {
-      removeEventListener('storage', onChange);
-    };
-  }, [key]);
-
-  return [value, setValue] as const;
+  return useBrowserState('local', key, initialState);
 };
 
 export default useLocalState;
