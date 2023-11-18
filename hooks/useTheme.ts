127d0eafdfee4577616ca87f183d604cef41d216
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-18 02:41:30 +0200

    Fix default theme

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import useLocalState from '~/hooks/useLocalState';
+import useMedia from '~/hooks/useMedia';
 
 type Theme = 'light' | 'dark';
 
 const useTheme = () => {
-  return useLocalState<Theme>('theme', 'dark');
+  const systemPreferredTheme = useMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
+  const [userPreferredTheme, setUserPreferredTheme] = useLocalState<Theme | null>('theme', null);
+  const [actualTheme, setActualTheme] = useState<Theme>('dark');
+
+  // If the user has not set a preferred theme, use the system's preferred theme
+  useEffect(() => {
+    if (userPreferredTheme) {
+      setActualTheme(userPreferredTheme);
+    } else {
+      setActualTheme(systemPreferredTheme);
+    }
+  }, [userPreferredTheme, systemPreferredTheme]);
+
+  return [actualTheme, setUserPreferredTheme] as const;
 };
 
 export default useTheme;
