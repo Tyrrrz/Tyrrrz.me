033eb81e1be6619b71474d2670fdf4ba870a528f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-18 23:59:37 +0200

    Improve theme switcher

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import useLocalState from '~/hooks/useLocalState';
 import useMedia from '~/hooks/useMedia';
 
@@ -7,18 +7,15 @@ type Theme = 'light' | 'dark';
 const useTheme = () => {
   const systemPreferredTheme = useMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
   const [userPreferredTheme, setUserPreferredTheme] = useLocalState<Theme | null>('theme', null);
-  const [actualTheme, setActualTheme] = useState<Theme>(systemPreferredTheme);
 
-  // If the user has not set a preferred theme, use the system's preferred theme
-  useEffect(() => {
-    if (userPreferredTheme) {
-      setActualTheme(userPreferredTheme);
-    } else {
-      setActualTheme(systemPreferredTheme);
-    }
-  }, [systemPreferredTheme, userPreferredTheme]);
-
-  return [actualTheme, setUserPreferredTheme] as const;
+  return useMemo(() => {
+    return {
+      systemPreferredTheme,
+      userPreferredTheme,
+      theme: userPreferredTheme || systemPreferredTheme,
+      setTheme: setUserPreferredTheme
+    };
+  }, [systemPreferredTheme, userPreferredTheme, setUserPreferredTheme]);
 };
 
 export default useTheme;
